d5220538c715bf3b13b7218b7f58f388e6ebc0ed
Dzmitry <maddal.privod@gmail.com>
2022-04-29 22:40:34 +0300

    Refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {BrowserRouter, HashRouter} from 'react-router-dom';
 import s from './App.module.css';
 import {AppRoutes} from './Routes/Routes';
 import {Header} from './Header/Header';
