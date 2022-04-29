35e1f468fb6a095ec986108c5fb1c5db73a895b2
Dzmitry <maddal.privod@gmail.com>
2022-04-29 22:50:38 +0300

    Refactoring

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -8,6 +8,8 @@ export const Header = () => {
         <div className={s.div}>
             <NavLink to={PATH.PROFILE} className={`${s.link} ${s.button}`}>Profile</NavLink>
             <NavLink to={PATH.TEST_PAGE} className={`${s.link} ${s.button}`}>Test page</NavLink>
+            <NavLink to={PATH.REGISTRATION} className={`${s.link} ${s.button}`}>Registration</NavLink>
+            <NavLink to={PATH.LOGIN} className={`${s.link} ${s.button}`}>Login</NavLink>
         </div>
     )
 }
