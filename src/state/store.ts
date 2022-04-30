cbbad277c6f4f260d2792d3d734591e579780246
Dzmitry <maddal.privod@gmail.com>
2022-04-30 18:57:57 +0300

    Refactoring

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,4 @@
-import {AnyAction, applyMiddleware, combineReducers, createStore} from 'redux'
+import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware, {ThunkDispatch} from 'redux-thunk'
 import {authReducer, LoginActionsType} from './auth-reducer';
 import {profileReducer} from './profile-reducer';
@@ -18,13 +18,13 @@ const rootReducer = combineReducers({
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
+export const useTypedDispatch = () => useDispatch<TypedDispatch>();
+
+//types
 export type AppRootStateType = ReturnType<typeof rootReducer>//
 export type AppActionType = LoginActionsType
 export type AppDispatch = typeof store.dispatch;//
 export type TypedDispatch = ThunkDispatch<AppRootStateType, any, AppActionType>;
-
-export const useTypedDispatch = () => useDispatch<TypedDispatch>();
-//export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 // @ts-ignore
 window.store = store;
