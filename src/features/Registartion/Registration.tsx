713df1e278be5bdc62894d28e9fee46e50459c70
MikolaiSib <MikolaiSib@gmail.com>
2022-04-30 16:00:49 +0700

    Commit 2 Mikolai add useFormik

--- src/features/Registartion/Registration.tsx
+++ src/features/Registartion/Registration.tsx
@@ -1,43 +1,63 @@
-import {Field, Form, Formik} from 'formik';
+import {useFormik} from 'formik';
 import s from './Registration.module.css'
 import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
 
 
 interface MyFormValues {
-    email: string
-    password: string
+    email?: string
+    password?: string
 }
 
 export const Registration = () => {
 
-    const initialValues: MyFormValues = { email: '', password: '' };
+    // const dispatch = useDispatch()
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validate: (values) => {
+            const errors: MyFormValues = {};
+            return errors;
+        },
+        onSubmit: values => {
+            // dispatch(loginTC(values))
+            alert(JSON.stringify(values))
+            formik.resetForm()
+        },
+    })
 
     return (
         <div className={s.div}>
             <div>Registration form</div>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={(values, actions) => {
-                    console.log({ values, actions });
-                    alert(JSON.stringify(values, null, 2));
-                    actions.setSubmitting(false);
-                }}
-            >
-                <Form className={s.form}>
+            <form onSubmit={formik.handleSubmit}>
+                <div className={s.form}>
                     <div>
                         <label htmlFor="email">email</label>
-                        <Field id="email" name="email" placeholder="email" />
+                        <Input
+                            id="email"
+                            name="email"
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                        />
                     </div>
                     <div>
                         <label htmlFor="password">password</label>
-                        <Field id="password" name="password" placeholder="password" />
+                        <Input
+                            id="password"
+                            name="password"
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
                     </div>
-                    {/*<div></div>*/}
                     <div>
                         <Button type="submit">sing up</Button>
                     </div>
-                </Form>
-            </Formik>
+                </div>
+            </form>
         </div>
     )
 }
