95327888445c7ff21c7a0b3bff467ca47d9ae0b9
MikolaiSib <MikolaiSib@gmail.com>
2022-04-30 15:00:32 +0700

    First commit Mikolai add jsx v1

--- src/features/Registartion/Registration.tsx
+++ src/features/Registartion/Registration.tsx
@@ -1,9 +1,43 @@
+import {Field, Form, Formik} from 'formik';
 import s from './Registration.module.css'
+import Button from "../../components/Button/Button";
+
+
+interface MyFormValues {
+    email: string
+    password: string
+}
 
 export const Registration = () => {
+
+    const initialValues: MyFormValues = { email: '', password: '' };
+
     return (
         <div className={s.div}>
-            Registration form
+            <div>Registration form</div>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={(values, actions) => {
+                    console.log({ values, actions });
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                }}
+            >
+                <Form className={s.form}>
+                    <div>
+                        <label htmlFor="email">email</label>
+                        <Field id="email" name="email" placeholder="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">password</label>
+                        <Field id="password" name="password" placeholder="password" />
+                    </div>
+                    {/*<div></div>*/}
+                    <div>
+                        <Button type="submit">sing up</Button>
+                    </div>
+                </Form>
+            </Formik>
         </div>
     )
 }
