a0defa9e778bad505c8b9961929fe29d27b682c0
Dzmitry <maddal.privod@gmail.com>
2022-04-30 12:01:26 +0300

    Api ready

---
+++ src/api/auth-api.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosResponse } from "axios"
+
+const instance = axios.create({
+    baseURL: 'http://localhost:7542/2.0/',
+    //baseURL: 'https://neko-back.herokuapp.com/2.0/',
+    withCredentials: true,
+})
+
+export const authAPI = {
+    login(data: LoginParamsType) {
+        return instance.post<LoginParamsType, AxiosResponse<ResponseType>>('auth/login', data)
+    },
+    me() {
+        return instance.post<ResponseType>('auth/me')
+    },
+    logout() {
+        return instance.delete<ResponseType>('auth/me')
+    }
+}
+
+//types
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe?: boolean
+}
+export type ResponseType = {
+    _id: string
+    email: string
+    name: string
+    avatar?: string
+    publicCardPacksCount: number
+    created: Date
+    updated: Date
+    isAdmin: boolean
+    verified: boolean
+    rememberMe: boolean
+    error?: string;
+}
+
