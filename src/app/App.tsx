60d5078689e9004b4f9d34bfaf3f9a57b048b31e
andreiryaskov <andreiryaskov@gmail.com>
2022-05-03 22:10:22 +1000

    profile page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import s from './App.module.css';
 import {AppRoutes} from './Routes/Routes';
 import {Header} from './Header/Header';
+import {Profile} from "../features/Profile/Profile";
 
 
 export const App = () => {
 
     return (
         <div className={s.app}>
-                <Header/>
-                <AppRoutes/>
+            <Header/>
+            <AppRoutes/>
+
         </div>
     )
 }
