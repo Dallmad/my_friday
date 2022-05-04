d0d21d428cf4d0f8e9686fd8db4ea036bd52b852
Dzmitry <maddal.privod@gmail.com>
2022-05-05 01:16:12 +0300

    Refactoring

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,11 +1,13 @@
 import {NavLink} from "react-router-dom";
 import {PATH} from '../Routes/Routes';
 import s from './Header.module.css'
+import {ErrorSnackbar} from '../../components/ErrorSnackbar/ErrorSnackbar';
 
 
 export const Header = () => {
     return (
         <div className={s.div}>
+            <ErrorSnackbar/>
             <NavLink to={PATH.PROFILE} className={`${s.link} ${s.button}`}>Profile</NavLink>
             <NavLink to={PATH.TEST_PAGE} className={`${s.link} ${s.button}`}>Test page</NavLink>
             <NavLink to={PATH.REGISTRATION} className={`${s.link} ${s.button}`}>Registration</NavLink>
