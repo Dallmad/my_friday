71a67042b777398fec6102e8989d4ab0578b6f88
Jaralva <zaingrazes@mail.ru>
2022-05-05 19:38:12 +0300

    Vintsent Profile 0.2 (Logout)

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -7,6 +7,7 @@ import {useEffect, useState} from 'react';
 import {setUser} from '../../state/auth-reducer';
 import EditableSpan from '../../components/EditableSpan/EditableSpan';
 import ava from '../../assets/images/ava.png'
+import {Logout} from "../../components/Logout/Logout";
 
 export const Profile = () => {
 
@@ -40,6 +41,7 @@ export const Profile = () => {
                                 onBlur={changeName}
                                 onEnter={changeName}
                 />
+                <Logout/>
             </div>
         </div>
     )
